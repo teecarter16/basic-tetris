dc833764a5afbe71e89a7b21c5bf03dd5af9f786
teecarter16 <tytycartet@gmail.com>
2021-06-13 11:36:17 -0700

    setting up divs

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const width = 10
+const ScoreDisplay = document.querySelector('#score')
+const StartBin = document.querySelector('#start-button')
+console.log(squares)
 
 
-    
 })
