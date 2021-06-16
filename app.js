bf4fa9589f42b24c37a6042efaa1304308ca2e30
teecarter16 <tytycartet@gmail.com>
2021-06-16 14:00:18 -0700

    skeleton of the game completed

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 const grid = document.querySelector('.grid')
 let squares = Array.from(document.querySelectorAll('.grid div'))
-const ScoreDisplay = document.querySelector('#score')
-const StartBin = document.querySelector('#start-button')
+const scoreDisplay = document.querySelector('#score')
+const startBtn = document.querySelector('#start-button')
 const width = 10
 let nextRandom = 0
+let timerId
+let score = 0
 //the tetrominoes
 const lTetromino = [
     [1, width +1, width*2+1,2],
@@ -66,7 +68,7 @@ function undraw(){
 }
 
 //make the Tetromino move down every second
-timerId = setInterval(moveDown, 1000)
+//timerId = setInterval(moveDown, 1000)
 //assigning keycodes
 function control(e) {
     if(e.keyCode === 37) {
@@ -100,6 +102,8 @@ function freeze() {
         currentPosition = 4
         draw()
         displayShape()
+        addScore()
+        gameOver()
     }
 }
 
@@ -166,6 +170,50 @@ function displayShape () {
         displaySquares[displayIndex + index].classList.add('tetromino')
     })
 }
+
+//adding funtionality to button
+startBtn.addEventListener('click', () => {
+if (timerId) {
+    clearInterval(timerId)
+    timerId = null
+} else{
+    draw()
+    timerId = setInterval(moveDown, 1000)
+    nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+    displayShape()
+    
+}
+})
+
+//adding score
+function addScore() {
+    for(let i = 0; i <199; i+=width) {
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+        if(row.every(index => squares[index].classList.contains('taken'))){
+            score +=10
+            scoreDisplay.innerHTML = score
+            row.forEach(index => {
+                squares[index].classList.remove('taken')
+                squares[index].classList.remove('tetromino')
+            })
+            const squaresRemoved = squares.splice(i, width)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
+        }
+
+    }
+}
+
+//game over function
+function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        scoreDisplay.innerHTML = 'end'
+        clearInterval(timerId)
+    }
+}
+
+
 
 
 
